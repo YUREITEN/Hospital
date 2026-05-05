b384f162c783fa4e92ea741b510f40886149bfb2
gg0urigoyal <gg0urigoyal@gmail.com>
2026-05-05 19:58:09 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-BH-HLMHy.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/projects/to edit from loavble/hospital/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/Hospital/assets/index-_ownL0d1.js"] }, "/": { filePath: "C:/projects/to edit from loavble/hospital/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/Hospital/assets/index-CPXmycyc.js"] } }, clientEntry: "/Hospital/assets/index-_ownL0d1.js" });
-export {
-  tsrStartManifest
-};
